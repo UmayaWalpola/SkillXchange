3eafcf6983fa9b07f184c84bb66bba76407f6a62
umayawalpola <umayawalpola@gmail.com>
2025-12-15 20:26:23 +0530

    configarations added for multi server usage. XAMMPP as default and docker locally implemented.

--- app/config/config.php
+++ app/config/config.php
@@ -15,6 +15,7 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'skillxchange');
+define('DB_PORT', 3306);
 
 // Start session for authentication
 if (session_status() === PHP_SESSION_NONE) {
