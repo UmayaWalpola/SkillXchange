b37a269cf79f645d4dff167aab915540f888d394
umayawalpola <umayawalpola@gmail.com>
2026-01-12 19:05:32 +0530

    permission issues fixed

--- app/config/db.local.php
+++ app/config/db.local.php
@@ -2,6 +2,6 @@
 // Local database configuration
 define('DB_HOST', '127.0.0.1');   // or 'localhost'
 define('DB_USER', 'root');        // your local MySQL username
-define('DB_PASS', 'root');        // your local MySQL password
-define('DB_NAME', 'skillxchange'); 
+define('DB_PASS', 'admin123');        // your local MySQL password
+define('DB_NAME', 'SkillXchange'); 
 define('DB_PORT', 3306);
