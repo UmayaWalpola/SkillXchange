148a4f563a5e1b0db4a8bd3aa5d276cce5868fbe
umayawalpola <umayawalpola@gmail.com>
2026-04-15 19:49:46 +0530

    mail.php added to facilitate mail sending

---
+++ app/config/mail.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('MAIL_HOST')) {
+    define('MAIL_HOST', 'smtp.example.com');
+}
+
+if (!defined('MAIL_USER')) {
+    define('MAIL_USER', 'test@example.com');
+}
+
+if (!defined('MAIL_PASS')) {
+    define('MAIL_PASS', 'password');
+}
+
+if (!defined('MAIL_PORT')) {
+    define('MAIL_PORT', 587);
+}
