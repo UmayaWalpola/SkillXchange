0b894609ecc71ec79931be1db9dd2aa045116de7
KithsaraSilva <kithsaradevinda@gmail.com>
2026-04-20 20:14:59 +0530

    Remove Feedback link from organization sidebar for organizations (orgs cannot receive feedback)

--- app/views/layouts/organization_sidebar.php
+++ app/views/layouts/organization_sidebar.php
@@ -19,8 +19,5 @@
                 <span>Wallet</span>
             </a>
             
-            <a href="<?= URLROOT ?>/Feedback/index" class="sidebar-item <?= (strpos($_SERVER['REQUEST_URI'], '/Feedback') !== false) ? 'active' : '' ?>">
-                <span>Feedback</span>
-            </a>
         </nav>
     </div>
